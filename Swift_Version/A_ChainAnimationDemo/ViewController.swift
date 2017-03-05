7fce7c89fff719bc7bd29f8149a14cf0ef05586b
Animax Deng <animax.my@gmail.com>
2017-03-04 23:10:34 -0600

    Submit simple Swift version UIView chain animation

---
+++ Swift_Version/A_ChainAnimationDemo/ViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  A_ChainAnimation
+//
+//  Created by Animax Deng on 3/4/17.
+//  Copyright © 2017 Animx. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: Implement UITableViewDelegate and UITableViewDataSource
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
+        
+        if let box = cell.viewWithTag(0) {
+            box.addAnimation(duration: 1.0, animation: {
+                box.alpha = 0.0
+            }).addAnimation(duration: 1.0, animation: {
+                box.alpha = 1.0
+            }).run()
+        }
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 90
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath as IndexPath, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        
+    }
+}
+
